b447dbc4aae4df2e39a1108a3d1ffd3c87c9034b
ratchek <git@ratchek.com>
2024-03-06 22:45:33 -0500

    Add git setup

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,3 +3,7 @@
 python3 -m venv --prompt {{ cookiecutter.env_prompt }} env
 source env/bin/activate
 pip install -r requirements.txt
+git init
+git add .
+git commit -m "Initial commit"
+git remote add origin {{ cookiecutter.github_url }}
