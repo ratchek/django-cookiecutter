63b9b1c50c33b4464484bad7ac0f074234e9aff4
ratchek <git@ratchek.com>
2024-03-09 15:16:35 -0500

    Fix cookiecutter variables

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.app_slug }}/apps.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.app_slug }}/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class {{ cookiecutter.app_class_name }}Config(AppConfig):
+class {{ cookiecutter.__app_class_name }}Config(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "{{ cookiecutter.app_name }}"
