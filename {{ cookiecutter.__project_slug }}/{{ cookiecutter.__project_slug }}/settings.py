97e13137ae33d407424be858840392fb900d4769
ratchek <git@ratchek.com>
2024-03-10 21:36:35 -0400

    Fix whitespace in settings.py

--- {{ cookiecutter.__project_slug }}/{{ cookiecutter.__project_slug }}/settings.py
+++ {{ cookiecutter.__project_slug }}/{{ cookiecutter.__project_slug }}/settings.py
@@ -43,10 +43,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    {% if cookiecutter.__django_allauth == "y" %}
+    {%- if cookiecutter.__django_allauth == "y" %}
     'allauth',
     'allauth.account',
-    {% endif %}
+    {%- endif %}
     users,
     '{{ cookiecutter.__app_slug }}',
 ]
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    {% if cookiecutter.__django_allauth == "y" %}
+    {%- if cookiecutter.__django_allauth == "y" %}
     "allauth.account.middleware.AccountMiddleware",
     {% endif %}
 ]
@@ -81,10 +81,10 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                {% if cookiecutter.__django_allauth == "y" %}
+                {%- if cookiecutter.__django_allauth == "y" %}
                 # `allauth` needs this from django
                 'django.template.context_processors.request',
-                {% endif %}
+                {%- endif %}
             ],
         },
     },
@@ -132,14 +132,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-{% if cookiecutter.__django_allauth == "y" %}
+{%- if cookiecutter.__django_allauth == "y" %}
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
     # `allauth` specific authentication methods, such as login by email
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
-{% endif %}
+{%- endif %}
 
 
 # Internationalization
