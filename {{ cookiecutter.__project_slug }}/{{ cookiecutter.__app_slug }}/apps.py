d626f228621eeb15c69b3bf1f2890881ca7b1505
ratchek <git@ratchek.com>
2024-03-10 21:15:17 -0400

    Fix app name

--- {{ cookiecutter.__project_slug }}/{{ cookiecutter.__app_slug }}/apps.py
+++ {{ cookiecutter.__project_slug }}/{{ cookiecutter.__app_slug }}/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class {{ cookiecutter.__app_class_name }}Config(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "{{ cookiecutter.app_name }}"
+    name = "{{ cookiecutter.__app_slug }}"
